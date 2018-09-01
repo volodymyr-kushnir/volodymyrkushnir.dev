81ceb1313963eb1431404f0e9707ceb4870cd6ac
Volodymyr Kushnir <volodymyr.kushnir@me.com>
2018-09-01 16:12:08 +0300

    Update service worker

--- sw.js
+++ sw.js
@@ -24,8 +24,12 @@ self.__precacheManifest = [
     "revision": "9910024255790206c64ba1fd3cf272eb"
   },
   {
+    "url": "assets/images/avatar-2012.svg",
+    "revision": "875d5e9fa16d17b2faa4784891cf39f4"
+  },
+  {
     "url": "assets/images/avatar.svg",
-    "revision": "435264b2a9656d605e7610233e52ac05"
+    "revision": "e3715d9b7856999eac310ab8a5de10b1"
   },
   {
     "url": "assets/images/favicon-192x192.png",
@@ -52,72 +56,76 @@ self.__precacheManifest = [
     "revision": "c3e4db5884523d386732e1fbc51b5402"
   },
   {
-    "url": "assets/images/volodymyr.kushnir-2017-square.jpg",
+    "url": "assets/images/volodymyr.kushnir-2012.jpg",
+    "revision": "5e9cde0c38fdd2f1c95f36dbf2a14557"
+  },
+  {
+    "url": "assets/images/volodymyr.kushnir-square.jpg",
     "revision": "f1d43fb4e8d7436d909b6fd61d2bcd85"
   },
   {
-    "url": "assets/images/volodymyr.kushnir-2017-square.png",
+    "url": "assets/images/volodymyr.kushnir-square.png",
     "revision": "6af613f4c199b523be00b6cb433fd5d4"
   },
   {
-    "url": "assets/images/volodymyr.kushnir-2017.jpg",
+    "url": "assets/images/volodymyr.kushnir.jpg",
     "revision": "7cb744ce232603c293c5cc75279697e7"
   },
   {
     "url": "assets/photos/323856_148624698589084_1191150964_o.jpg",
-    "revision": "1402145ef793fbcf657eaf5c7f944bca"
+    "revision": "23205ffa1a41b16a0b12357ba4513e19"
   },
   {
     "url": "assets/photos/323868_148639808587573_1648505013_o.jpg",
-    "revision": "7e39462d69434d6dae37bf5f97a218b7"
+    "revision": "54ebc728f45f2645a32adc022c9bffbb"
   },
   {
     "url": "assets/photos/324164_148624265255794_446501669_o.jpg",
-    "revision": "5c01966483c269fc811f1f0a22fa769c"
+    "revision": "9c10847e25fe04bb4481836c0bab5986"
   },
   {
     "url": "assets/photos/327086_148624111922476_1330178874_o.jpg",
-    "revision": "81a021100b6691e9ee3ac8cef936e5e3"
+    "revision": "da8237b2d9167f0f5836275e2dc4549b"
   },
   {
     "url": "assets/photos/329642_148624421922445_2060074436_o.jpg",
-    "revision": "eb1c87f9f28e95c4680cd1ce2f67771b"
+    "revision": "4db750c0098bea4b9393d8934fcff31d"
   },
   {
     "url": "assets/photos/331152_148624551922432_1869335234_o.jpg",
-    "revision": "14432a4b1aacff2c17783d38ea8e1983"
+    "revision": "3793bb9bb89b62bb62a7db1d079a1d8c"
   },
   {
     "url": "assets/photos/412746_158114720973415_949035392_o.jpg",
-    "revision": "0e1d3a2e703af34e6631c1673c455630"
+    "revision": "5e1f873f1420195e58be4cc61f963845"
   },
   {
     "url": "assets/photos/461737_173999722718248_1691527332_o.jpg",
-    "revision": "b5e74f5afe8c890a76c22d97cdd29f43"
+    "revision": "10c3d6ec0f27df301a9493c979e1db0f"
   },
   {
     "url": "assets/photos/466052_170309293087291_793458215_o.jpg",
-    "revision": "516294d7a72c5e6c18f30b36ae905647"
+    "revision": "6433e08492057dd33332dad2303f1474"
   },
   {
     "url": "assets/photos/467536_170309526420601_636909857_o.jpg",
-    "revision": "aafebe16402efe94b302ef6e94ffdc12"
+    "revision": "1c9556e4b8ff9da875150aed3b806fe6"
   },
   {
     "url": "assets/photos/470468_170309399753947_1333978849_o.jpg",
-    "revision": "548ae4f57e6d1cd995510b1af744a7b4"
+    "revision": "294a4019c2420e475ed4ef0a97523ed8"
   },
   {
     "url": "assets/photos/471025_173999919384895_1249822733_o.jpg",
-    "revision": "29bc44adc14df9146de8d7d920ee716c"
+    "revision": "42411e3ed7395e48f2633c9cf19a1b7d"
   },
   {
     "url": "assets/photos/473110_173999982718222_1427466111_o.jpg",
-    "revision": "3f20a60a31ba8b4fbb30bc9130b15f31"
+    "revision": "55e0d4bdccd706bb14bf405bf82a743e"
   },
   {
     "url": "assets/photos/476877_173999862718234_1686531489_o.jpg",
-    "revision": "0224d9f15174bed22e28476121b76b89"
+    "revision": "fd27e0acdbcf14033b63235e907fe2b3"
   },
   {
     "url": "assets/sounds/meh.mp3",
@@ -125,7 +133,7 @@ self.__precacheManifest = [
   },
   {
     "url": "assets/stylesheets/base.css",
-    "revision": "798ea6b005642b8460b18141b9fe1a6f"
+    "revision": "b17188b9bfd23cc431b6ea0d8e6d1b9d"
   },
   {
     "url": "favicon.ico",
@@ -133,15 +141,31 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "ff5dfc4b604359845c5548fa8e8e3e15"
+    "revision": "9ea016bf31f7d83b3883aa1d6c70724b"
+  },
+  {
+    "url": "posts/index.html",
+    "revision": "08196cbea8cc9d6b60e23070d5ca5adc"
+  },
+  {
+    "url": "posts/introduction-letter-to-binary-studio/assets/photos/1244180_457131501071734_1731017764_o.jpg",
+    "revision": "e54ff1f81e90b5c8b6caff626cef069b"
+  },
+  {
+    "url": "posts/introduction-letter-to-binary-studio/assets/stylesheets/base.css",
+    "revision": "44eb94af75ae967239aac8f0e42be2bd"
+  },
+  {
+    "url": "posts/introduction-letter-to-binary-studio/index.html",
+    "revision": "cd012d20edbc79d7b73a811c35bdaf39"
   },
   {
     "url": "ru-ru/index.html",
-    "revision": "d13d06f9eeee93985777b4c0d0e7488c"
+    "revision": "7d1ca571aeccbe8898a8708e97e4cb2a"
   },
   {
     "url": "uk-ua/index.html",
-    "revision": "e6e1d657da96eadefb42e28f7ea176b7"
+    "revision": "44046727023b0e5c50b0df9126418586"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
